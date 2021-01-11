fcfc767aadc4faec6f63aedd034c86ae29c61a1a
msoulard <soulardmimi@gmail.com>
2021-01-11 12:02:53 +0100

    11/01/2021

---
+++ build-Supervision_Rondier-Desktop_Qt_5_15_0_GCC_64bit-Debug/moc_communicationadb.cpp
@@ -0,0 +1,129 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'communicationadb.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../ControleurDeRonde_2020/communicationadb.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'communicationadb.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_CommunicationADB_t {
+    QByteArrayData data[5];
+    char stringdata0[119];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_CommunicationADB_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_CommunicationADB_t qt_meta_stringdata_CommunicationADB = {
+    {
+QT_MOC_LITERAL(0, 0, 16), // "CommunicationADB"
+QT_MOC_LITERAL(1, 17, 32), // "onQProcessListeAppareilsFinished"
+QT_MOC_LITERAL(2, 50, 0), // ""
+QT_MOC_LITERAL(3, 51, 33), // "onQProcessListeAppareilsReady..."
+QT_MOC_LITERAL(4, 85, 33) // "onQProcessSynchronisationFini..."
+
+    },
+    "CommunicationADB\0onQProcessListeAppareilsFinished\0"
+    "\0onQProcessListeAppareilsReadyRead\0"
+    "onQProcessSynchronisationFinished"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_CommunicationADB[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       3,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   29,    2, 0x08 /* Private */,
+       3,    0,   30,    2, 0x08 /* Private */,
+       4,    0,   31,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void CommunicationADB::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<CommunicationADB *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->onQProcessListeAppareilsFinished(); break;
+        case 1: _t->onQProcessListeAppareilsReadyRead(); break;
+        case 2: _t->onQProcessSynchronisationFinished(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject CommunicationADB::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_CommunicationADB.data,
+    qt_meta_data_CommunicationADB,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *CommunicationADB::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *CommunicationADB::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CommunicationADB.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int CommunicationADB::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 3)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 3;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
