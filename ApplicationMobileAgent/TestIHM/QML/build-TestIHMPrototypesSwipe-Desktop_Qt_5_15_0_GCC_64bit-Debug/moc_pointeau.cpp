32d19798f3f71b65cd4b8b542a91b5c10f9f33fa
msoulard <soulardmimi@gmail.com>
2021-03-11 18:00:34 +0100

    1/03/2021

--- ApplicationMobileAgent/TestIHM/QML/build-TestIHMPrototypesSwipe-Desktop_Qt_5_15_0_GCC_64bit-Debug/moc_pointeau.cpp
+++ ApplicationMobileAgent/TestIHM/QML/build-TestIHMPrototypesSwipe-Desktop_Qt_5_15_0_GCC_64bit-Debug/moc_pointeau.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Pointeau_t {
-    QByteArrayData data[5];
-    char stringdata0[37];
+    QByteArrayData data[9];
+    char stringdata0[71];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,13 +33,18 @@ struct qt_meta_stringdata_Pointeau_t {
 static const qt_meta_stringdata_Pointeau_t qt_meta_stringdata_Pointeau = {
     {
 QT_MOC_LITERAL(0, 0, 8), // "Pointeau"
-QT_MOC_LITERAL(1, 9, 14), // "changerCouleur"
-QT_MOC_LITERAL(2, 24, 0), // ""
-QT_MOC_LITERAL(3, 25, 1), // "i"
-QT_MOC_LITERAL(4, 27, 9) // "horodater"
+QT_MOC_LITERAL(1, 9, 11), // "lieuChanged"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 11), // "etatChanged"
+QT_MOC_LITERAL(4, 34, 14), // "changerCouleur"
+QT_MOC_LITERAL(5, 49, 1), // "i"
+QT_MOC_LITERAL(6, 51, 9), // "horodater"
+QT_MOC_LITERAL(7, 61, 4), // "lieu"
+QT_MOC_LITERAL(8, 66, 4) // "etat"
 
     },
-    "Pointeau\0changerCouleur\0\0i\0horodater"
+    "Pointeau\0lieuChanged\0\0etatChanged\0"
+    "changerCouleur\0i\0horodater\0lieu\0etat"
 };
 #undef QT_MOC_LITERAL
 
@@ -49,20 +54,36 @@ static const uint qt_meta_data_Pointeau[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
-       0,    0, // properties
+       4,   14, // methods
+       2,   40, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   34,    2, 0x06 /* Public */,
+       3,    0,   35,    2, 0x06 /* Public */,
 
  // methods: name, argc, parameters, tag, flags
-       1,    1,   24,    2, 0x02 /* Public */,
-       4,    0,   27,    2, 0x02 /* Public */,
+       4,    1,   36,    2, 0x02 /* Public */,
+       6,    0,   39,    2, 0x02 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void,
 
  // methods: parameters
-    QMetaType::QString, QMetaType::Int,    3,
+    QMetaType::QString, QMetaType::Int,    5,
     QMetaType::Void,
+
+ // properties: name, type, flags
+       7, QMetaType::QString, 0x00495103,
+       8, QMetaType::QString, 0x00495103,
+
+ // properties: notify_signal_id
+       0,
+       1,
 
        0        // eod
 };
@@ -73,12 +94,52 @@ void Pointeau::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, vo
         auto *_t = static_cast<Pointeau *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: { QString _r = _t->changerCouleur((*reinterpret_cast< int(*)>(_a[1])));
+        case 0: _t->lieuChanged(); break;
+        case 1: _t->etatChanged(); break;
+        case 2: { QString _r = _t->changerCouleur((*reinterpret_cast< int(*)>(_a[1])));
             if (_a[0]) *reinterpret_cast< QString*>(_a[0]) = std::move(_r); }  break;
-        case 1: _t->horodater(); break;
+        case 3: _t->horodater(); break;
         default: ;
         }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (Pointeau::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Pointeau::lieuChanged)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (Pointeau::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Pointeau::etatChanged)) {
+                *result = 1;
+                return;
+            }
+        }
     }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty) {
+        auto *_t = static_cast<Pointeau *>(_o);
+        Q_UNUSED(_t)
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< QString*>(_v) = _t->lieu(); break;
+        case 1: *reinterpret_cast< QString*>(_v) = _t->etat(); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::WriteProperty) {
+        auto *_t = static_cast<Pointeau *>(_o);
+        Q_UNUSED(_t)
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: _t->setLieu(*reinterpret_cast< QString*>(_v)); break;
+        case 1: _t->setEtat(*reinterpret_cast< QString*>(_v)); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::ResetProperty) {
+    }
+#endif // QT_NO_PROPERTIES
 }
 
 QT_INIT_METAOBJECT const QMetaObject Pointeau::staticMetaObject = { {
@@ -110,15 +171,44 @@ int Pointeau::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 4;
+    }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty || _c == QMetaObject::WriteProperty
+            || _c == QMetaObject::ResetProperty || _c == QMetaObject::RegisterPropertyMetaType) {
+        qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::QueryPropertyDesignable) {
+        _id -= 2;
+    } else if (_c == QMetaObject::QueryPropertyScriptable) {
+        _id -= 2;
+    } else if (_c == QMetaObject::QueryPropertyStored) {
+        _id -= 2;
+    } else if (_c == QMetaObject::QueryPropertyEditable) {
+        _id -= 2;
+    } else if (_c == QMetaObject::QueryPropertyUser) {
         _id -= 2;
     }
+#endif // QT_NO_PROPERTIES
     return _id;
+}
+
+// SIGNAL 0
+void Pointeau::lieuChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void Pointeau::etatChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
--- BDDs/StationAccueil/BDDStationAccueil.sql
+++ BDDs/StationAccueil/BDDStationAccueil.sql
@@ -1,98 +1,105 @@
 CREATE DATABASE IF NOT EXISTS `StationAccueil` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `StationAccueil`;
 
-CREATE TABLE `PEUTFAIRE` (
-  `idagent` VARCHAR(42),
-  `idronde` VARCHAR(42),
-  PRIMARY KEY (`idagent`, `idronde`)
+CREATE TABLE `peut_faire` (
+  `id_agent` INTEGER,
+  `id_ronde` INTEGER,
+  PRIMARY KEY (`id_agent`, `id_ronde`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `RONDES` (
-  `idronde` VARCHAR(42),
-  `nom` VARCHAR(42),
+CREATE TABLE `rondes` (
+  `id_ronde` INTEGER,
+  `nom` VARCHAR(30),
   `statut` VARCHAR(42),
-  PRIMARY KEY (`idronde`)
+  PRIMARY KEY (`id_ronde`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `COMPORTE` (
-  `idronde` VARCHAR(42),
-  `idpointeau` VARCHAR(42),
-  `ordre` VARCHAR(42),
+CREATE TABLE `comporte` (
+  `id_ronde` INTEGER,
+  `id_pointeau` INTEGER,
+  `ordre` INTEGER,
   `tempsmini` VARCHAR(42),
   `tempsmaxi` VARCHAR(42),
-  PRIMARY KEY (`idronde`, `idpointeau`)
+  PRIMARY KEY (`id_ronde`, `id_pointeau`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `RESPONSABLES` (
-  `idresponsable` VARCHAR(42),
-  `nom` VARCHAR(42),
-  `prenom` VARCHAR(42),
-  `numbadge` VARCHAR(42),
-  `login` VARCHAR(42),
-  `mdp` VARCHAR(42),
+CREATE TABLE `responsables` (
+  `id_responsable` INTEGER,
+  `nom` VARCHAR(30),
+  `prenom` VARCHAR(30),
+  `numbadge` VARCHAR(10),
+  `login` VARCHAR(60),
+  `mdp` VARCHAR(30),
   `statut` VARCHAR(42),
-  PRIMARY KEY (`idresponsable`)
+  PRIMARY KEY (`id_responsable`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `AGENTS` (
-  `idagent` VARCHAR(42),
-  `nom` VARCHAR(42),
-  `prenom` VARCHAR(42),
-  `numbadge` VARCHAR(42),
+CREATE TABLE `agents` (
+  `id_agent` INTEGER,
+  `nom` VARCHAR(30),
+  `prenom` VARCHAR(30),
+  `numbadge` VARCHAR(10),
   `statut` VARCHAR(42),
-  PRIMARY KEY (`idagent`)
+  PRIMARY KEY (`id_agent`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `AETEEFFECTUEEPAR` (
-  `idronde` VARCHAR(42),
-  `idagent` VARCHAR(42),
+CREATE TABLE `a_ete_effectuee_par` (
+  `id_ronde` INTEGER,
+  `id_agent` INTEGER,
   `date` VARCHAR(42),
   `heure` VARCHAR(42),
-  PRIMARY KEY (`idronde`, `idagent`)
+  PRIMARY KEY (`id_ronde`, `id_agent`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `POINTEAUX` (
-  `idpointeau` VARCHAR(42),
-  `designation` VARCHAR(42),
+CREATE TABLE `pointeaux` (
+  `id_pointeau` INTEGER,
+  `designation` VARCHAR(30),
   `statut` VARCHAR(42),
-  `tag_mifare` VARCHAR(42),
-  PRIMARY KEY (`idpointeau`)
+  `tag_mifare` VARCHAR(10),
+  PRIMARY KEY (`id_pointeau`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `ANOMALIES` (
-  `idanomalie` VARCHAR(42),
-  `description` VARCHAR(42),
-  `photos` VARCHAR(42),
-  PRIMARY KEY (`idanomalie`)
+CREATE TABLE `anomalies` (
+  `id_anomalie` INTEGER,
+  `description` VARCHAR(150),
+  `id_photo` INTEGER,
+  PRIMARY KEY (`id_anomalie`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `AETESCANNE` (
-  `idpointeau` VARCHAR(42),
-  `idagent` VARCHAR(42),
-  `idronde` VARCHAR(42),
-  `idanomalie` VARCHAR(42),
-  `ordre` VARCHAR(42),
+CREATE TABLE `a_ete_scanne` (
+  `id_pointeau` INTEGER,
+  `id_agent` INTEGER,
+  `id_ronde` INTEGER,
+  `id_anomalie` INTEGER,
+  `ordre` INTEGER,
   `temps` VARCHAR(42),
-  PRIMARY KEY (`idpointeau`, `idagent`, `idronde`, `idanomalie`)
+  PRIMARY KEY (`id_pointeau`, `id_agent`, `id_ronde`, `id_anomalie`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `LIEUX` (
-  `idlieux` VARCHAR(42),
-  `batiment` VARCHAR(42),
-  `etage` VARCHAR(42),
-  `emplacement` VARCHAR(42),
-  `idpointeau` VARCHAR(42),
-  PRIMARY KEY (`idlieux`)
+CREATE TABLE `lieux` (
+  `id_lieu` INTEGER,
+  `batiment` VARCHAR(30),
+  `etage` VARCHAR(30),
+  `emplacement` VARCHAR(30),
+  `id_pointeau` INTEGER,
+  PRIMARY KEY (`id_lieu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `PEUTFAIRE` ADD FOREIGN KEY (`idronde`) REFERENCES `RONDES` (`idronde`);
-ALTER TABLE `PEUTFAIRE` ADD FOREIGN KEY (`idagent`) REFERENCES `AGENTS` (`idagent`);
-ALTER TABLE `COMPORTE` ADD FOREIGN KEY (`idpointeau`) REFERENCES `POINTEAUX` (`idpointeau`);
-ALTER TABLE `COMPORTE` ADD FOREIGN KEY (`idronde`) REFERENCES `RONDES` (`idronde`);
-ALTER TABLE `AETEEFFECTUEEPAR` ADD FOREIGN KEY (`idagent`) REFERENCES `AGENTS` (`idagent`);
-ALTER TABLE `AETEEFFECTUEEPAR` ADD FOREIGN KEY (`idronde`) REFERENCES `RONDES` (`idronde`);
-ALTER TABLE `AETESCANNE` ADD FOREIGN KEY (`idanomalie`) REFERENCES `ANOMALIES` (`idanomalie`);
-ALTER TABLE `AETESCANNE` ADD FOREIGN KEY (`idronde`) REFERENCES `RONDES` (`idronde`);
-ALTER TABLE `AETESCANNE` ADD FOREIGN KEY (`idagent`) REFERENCES `AGENTS` (`idagent`);
-ALTER TABLE `AETESCANNE` ADD FOREIGN KEY (`idpointeau`) REFERENCES `POINTEAUX` (`idpointeau`);
-ALTER TABLE `LIEUX` ADD FOREIGN KEY (`idpointeau`) REFERENCES `POINTEAUX` (`idpointeau`);
+CREATE TABLE `photos` (
+  `id_photo` INTEGER,
+  `chemin` VARCHAR(300),
+  PRIMARY KEY (`id_photo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `peut_faire` ADD FOREIGN KEY (`id_ronde`) REFERENCES `rondes` (`id_ronde`);
+ALTER TABLE `peut_faire` ADD FOREIGN KEY (`id_agent`) REFERENCES `agents` (`id_agent`);
+ALTER TABLE `comporte` ADD FOREIGN KEY (`id_pointeau`) REFERENCES `pointeaux` (`id_pointeau`);
+ALTER TABLE `comporte` ADD FOREIGN KEY (`id_ronde`) REFERENCES `rondes` (`id_ronde`);
+ALTER TABLE `a_ete_effectuee_par` ADD FOREIGN KEY (`id_agent`) REFERENCES `agents` (`id_agent`);
+ALTER TABLE `a_ete_effectuee_par` ADD FOREIGN KEY (`id_ronde`) REFERENCES `rondes` (`id_ronde`);
+ALTER TABLE `anomalies` ADD FOREIGN KEY (`id_photo`) REFERENCES `photos` (`id_photo`);
+ALTER TABLE `a_ete_scanne` ADD FOREIGN KEY (`id_anomalie`) REFERENCES `anomalies` (`id_anomalie`);
+ALTER TABLE `a_ete_scanne` ADD FOREIGN KEY (`id_ronde`) REFERENCES `rondes` (`id_ronde`);
+ALTER TABLE `a_ete_scanne` ADD FOREIGN KEY (`id_agent`) REFERENCES `agents` (`id_agent`);
+ALTER TABLE `a_ete_scanne` ADD FOREIGN KEY (`id_pointeau`) REFERENCES `pointeaux` (`id_pointeau`);
+ALTER TABLE `lieux` ADD FOREIGN KEY (`id_pointeau`) REFERENCES `pointeaux` (`id_pointeau`);
